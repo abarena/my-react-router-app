0a047e37af7e90cae6b08648de95fda93cfcf245
abarena <angelbarena89@gmail.com>
2025-03-04 15:12:03 -0600

    Implement clientLoader for fetching contacts and update App component to display contacts list

--- app/root.tsx
+++ app/root.tsx
@@ -9,8 +9,15 @@ import {
 import type { Route } from "./+types/root";
 
 import appStylesHref from "./app.css?url";
+import { getContacts } from "./data";
 
-export default function App() {
+export async function clientLoader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
+
+export default function App({ loaderData }: Route.ComponentProps) {
+  const { contacts } = loaderData;
   return (
     <>
       <div id="sidebar">
@@ -31,14 +38,30 @@ export default function App() {
           </Form>
         </div>
         <nav>
+        {contacts.length ? (
           <ul>
-            <li>
-              <Link to={`/contacts/1`}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={`/contacts/2`}>Your Friend</Link>
-            </li>
+            {contacts.map((contact) => (
+              <li key={contact.id}>
+                <Link to={`contacts/${contact.id}`}>
+                  {contact.first || contact.last ? (
+                    <>
+                      {contact.first} {contact.last}
+                    </>
+                  ) : (
+                    <i>No Name</i>
+                  )}
+                  {contact.favorite ? (
+                    <span>★</span>
+                  ) : null}
+                </Link>
+              </li>
+            ))}
           </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="detail">
@@ -96,5 +119,14 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         </pre>
       )}
     </main>
+  );
+}
+
+export function HydrateFallback() {
+  return (
+    <div id="loading-splash">
+      <div id="loading-splash-spinner" />
+      <p>Loading, please wait...</p>
+    </div>
   );
 }
