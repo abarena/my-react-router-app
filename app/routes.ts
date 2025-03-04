1f5e6a93038510444174928aebe05788420e7e07
abarena <angelbarena89@gmail.com>
2025-03-04 15:36:01 -0600

    Add sidebar layout with contact list and about page

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,9 @@
-import { route, type RouteConfig } from "@react-router/dev/routes";
+import { index, layout, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-  route("contacts/:conetactId", "routes/contact.tsx"),
+  layout("layouts/sidebar.tsx", [
+    index("routes/home.tsx"),
+    route("contacts/:conetactId", "routes/contact.tsx"),
+  ]),
+  route("about", "routes/about.tsx"),
 ] satisfies RouteConfig;
